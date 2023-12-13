f94bc002290b957b025d8c8484633ea9876ecb04
Eashswain <115333645+Eashswain@users.noreply.github.com>
2023-12-13 16:37:29 +0000

    code config done

---
+++ 01_basic/variable.js
@@ -0,0 +1,12 @@
+const accountId="1"
+let accountEmail="eashswain3001@gmail.com"
+var accountPassword="12222"
+accountcity="Kolkata"
+
+/*
+Not prefer to use var in variable because var shows problem in Block scope and function scope
+*/
+accountEmail="d412bmsit.in"
+accountPassword="123"
+accountcity="Bnagalore"
+console.table([accountId,accountEmail,accountPassword,accountcity])
