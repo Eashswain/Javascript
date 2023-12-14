18f8271a4233ee5e914e16a1a6460888fe993a05
Eashswain <115333645+Eashswain@users.noreply.github.com>
2023-12-14 13:38:48 +0000

    Datatypes

---
+++ 01_basic/02_dataypes.js
@@ -0,0 +1,19 @@
+//alert  //we are using the nodejs,not the browser.therefore cant use alert directly
+console.log(3+3)
+console.log("Easha")
+// numbers=>2 to power 53
+//Bigint
+//boolean=>true
+//let
+//undefined
+//NULL
+//undefined
+//object
+let Name="Esha"
+let age=20
+let email="eashswain3001@gmail.com"
+let isLoggedin=true
+
+console.log(typeof "Esha")
+console.log(typeof undefined) //we will get output as undefined becuz it is itself a type
+console.log(typeof null) //type of null is object
